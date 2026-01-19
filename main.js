555d352c27eb2d0db70914b9c99714f0efff571a
Derek Parsons <derekparsons718@gmail.com>
2026-01-18 17:35:30 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const routes = [
+  "#home",
+  "#new-move-ins",
+  "#emergency-prep",
+  "#resources",
+  "#contact",
+];
+
+window.addEventListener("hashchange", () => {
+  routeTo(window.location.hash);
+});
+
+window.addEventListener("load", () => {
+  routeTo(window.location.hash);
+});
+
+/**
+ * @param {string} view
+ * @returns {void}
+ */
+function routeTo(newView) {
+  console.log("Navigated to:", newView);
+  if (!routes.includes(newView)) {
+    window.location.hash = "#home";
+    return;
+  }
+  for (const route of routes) {
+    const view = document.querySelector(route);
+    view.style.display = route === newView ? "block" : "none";
+    const navLink = document.querySelector(`a.nav-link[href="${route}"]`);
+    navLink.classList.toggle("active", route === newView);
+  }
+}
